b71f892470a71d706e61928961d3196a0614d2b2
Peniel Tambama <tambama.peniel@outlook.com>
2025-10-02 10:38:05 +0200

    telegram

--- Pfuma/Core/Configuration/NotificationSettings.cs
+++ Pfuma/Core/Configuration/NotificationSettings.cs
@@ -3,8 +3,13 @@ namespace Pfuma.Core.Configuration;
 public class NotificationSettings
 {
     public bool EnableLog { get; set; } = false;
-    public bool EnableTelegram { get; set; } = false;
-    public bool NotifyLiquiditySweep { get; set; } = false;
+    public bool SendLiquidity { get; set; } = false;
+    public bool SendCycles { get; set; } = false;
+    public bool SendSMT { get; set; } = false;
+    public bool SendVenom { get; set; } = false;
+    public bool SendCISD { get; set; } = false;
+    public bool SendOrderBlock { get; set; } = false;
+    public bool SendInsideKeyLevel { get; set; } = false;
     public string TelegramChatId { get; set; } = "5631623580";
     public string TelegramToken { get; set; } = "7507336625:AAHM4oYlg_5XIjzzCNFCR_oyLu1Y69qkvns";
 }
